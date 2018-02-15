b465e5724d06c6b3ba04bb515001dee6814ce874
Dawon94 <ssop6403@gmail.com>
2018-02-15 17:44:45 +0900

    add graphic

--- HangMan/src/Game.java
+++ HangMan/src/Game.java
@@ -1,45 +1,48 @@
 import java.util.Scanner;
+import acm.program.*;
 
-public class Game {
+public class Game extends ConsoleProgram{
 	public static int round = 0;
-	private WordList wordList;
-	private Scanner sc;
-	
-	Game(){
-		wordList = WordList.getInstance();
-		sc = new Scanner(System.in);
-	}
-	
+	private WordList wordList; 
+	private HangmanCanvas canvas;
+	private StringBuilder incorrectLetter;
+	private StringBuilder displayedString;
+
 	private void hangman() {
 		String answer = wordList.getWord();
 		char[] problemWord = answer.toCharArray();
 		boolean[] guessed = new boolean[problemWord.length];
 		int leftGuessing = 8;
 		while(leftGuessing != 0) {
-			System.out.print("The word now looks like this:  ");
+			print("The word now looks like this:  ");
 			boolean isCorrect = true;
+			displayedString = new StringBuilder();
 			for (int i = 0; i < problemWord.length; i++) {
 				if(guessed[i]) {
-					System.out.print(problemWord[i]+ " ");
+					print(problemWord[i]+ " ");
+					displayedString.append(problemWord[i] + " ");
 				}else {
-					System.out.print("_ ");
+					print("_ ");
+					displayedString.append("_ ");
 					isCorrect = false;
 				}
 			}
-			
+
+			canvas.displayWord(displayedString.toString());
+
 			if(isCorrect) {
-				System.out.println("");
-				System.out.println("You guessed the word : " + answer);
-				System.out.println("You win");
-				System.out.println("Go to the next Game");
+				println("");
+				println("You guessed the word : " + answer);
+				println("You win");
+				println("Go to the next Game");
 				break;
 			}
-			
-			System.out.println("");
-			System.out.println("You have " + leftGuessing + " guesses left.");
-			System.out.print("Your guess:");
-			String strLetter = sc.next();
-			System.out.println("");
+
+			println("");
+			println("You have " + leftGuessing + " guesses left.");
+			print("Your guess:");
+			String strLetter = readLine();
+			println("");
 			char letter = strLetter.toCharArray()[0];
 			boolean isGuessed = false;
 			for (int i = 0; i < problemWord.length; i++) {
@@ -48,29 +51,42 @@ public class Game {
 					isGuessed = true;
 				}
 			}
-			
+
 			if (isGuessed == false) {
-				System.out.println("There are no" + letter + "'s in the word");
+				println("There are no" + letter + "'s in the word");
 				leftGuessing -= 1;
+				incorrectLetter.append(letter);
+				canvas.noteIncorrectGuess(incorrectLetter.toString());
 				if (leftGuessing == 0) {
-					System.out.println("You loose");
-					System.out.println("Go to the next Game");
+					println("You loose");
+					println("Go to the next Game");
+					println("");
+					canvas.reset();
 				}
 				continue;
 			}else {
-				System.out.println("The guess is Correct");	
+				println("The guess is Correct");	
 			}
-	 		}
 		}
-		
-	
-	public void start() {
+	}
+
+
+	public void run() {
+		wordList = WordList.getInstance();
 		while(wordList.getWordListSize() > round) {
+			canvas.reset();
+			incorrectLetter = new StringBuilder();
 			hangman();
 			round++;
 		}
 	}
-	
-	
+
+	public void init() {
+		canvas = new HangmanCanvas();
+		add(canvas);
+
+	}
+
+
 
 }
